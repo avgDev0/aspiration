f236fba7ddcbe435c48b88abbe6de12af24d9d3f
Luis Mercado <mercadoluisdaniel@gmail.com>
2021-09-08 19:45:14 -0500

    Introducing MUI theme

--- src/App.js
+++ src/App.js
@@ -1,21 +1,44 @@
+import React from 'react'
+import { ThemeProvider, CssBaseline } from '@material-ui/core';
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  pallete: {
+    primary: {
+      main: '#f1eaea',
+    },
+    secondary: {
+      main: '#c5e1a5',
+    },
+    warning: {
+      main: '#d32f2f',
+    },
+    info: {
+      main: '#d1c4c4',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
