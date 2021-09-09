1a64780cf5ee06625f199443ad1a7dace6db6da9
Luis Mercado <mercadoluisdaniel@gmail.com>
2021-09-09 01:40:46 -0500

    Changes to Wrapper layout and eslint changes to index.js file

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import PropTypes from 'prop-types'
 
 const useStyles = makeStyles({
   container: {
     width: '100%',
     height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 })
 
@@ -12,4 +16,8 @@ export default function Wrapper({ children }) {
   const classes = useStyles()
 
   return <div className={classes.container}>{children}</div>
+}
+
+Wrapper.propTypes = {
+  children: PropTypes.node.isRequired,
 }
